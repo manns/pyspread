9151be3e71bc8a125495f9fae35b0e9919e7b345
Martin Manns <mmanns@gmx.net>
2019-07-25 15:38:59 +0200

    Orga files updated for pyspread2 repo

--- pyspread.sh
+++ pyspread.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#export PYTHONPATH=$PYTHONPATH:/home/mn/prog/Phoenix
-
 # Calls pyspread from top level folder of extracted tarball
 export PYTHONPATH=$PYTHONPATH:.
 python3 ./src/pyspread.py $@
